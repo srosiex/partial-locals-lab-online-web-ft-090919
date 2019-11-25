26be5c352c1d0145ba6a2ba15ae32bdc6bb33c29
Sarah Henderson <sarahrose.henderson@gmail.com>
2019-11-24 20:02:46 -0600

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,15 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+
+  def self.search(search)
+    if search.present?
+      where('NAME like ?', "%#{search}%")
+    else
+      self.all
+    end
+  end
+
+
 end
